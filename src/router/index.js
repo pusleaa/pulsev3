65072596602fdc93c733f08fd4aa65851e5642dc
pusleaa <127901965+pusleaa@users.noreply.github.com>
2023-03-28 22:57:43 +0100

    a

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ConnectView from '../views/ConnectView.vue'
-import SucesstView from '../views/SuccessView.vue'
+// import SucesstView from '../views/SuccessView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },{
-    path: '/success',
-    name: 'success',
-    component: SuccessView
   },
+  // {
+  //   path: '/success',
+  //   name: 'success',
+  //   component: SuccessView
+  // },
   {
     path: '/connect',
     name: 'connect',
